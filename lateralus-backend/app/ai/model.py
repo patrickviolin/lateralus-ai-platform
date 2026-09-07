e4d315fda682120d356bb49b6f97b3ccd7510069
patrick-jk <patrick.pviolin@senacsp.edu.br>
2026-09-06 21:38:56 -0300

    add: structured response for tool calling

--- lateralus-backend/app/ai/model.py
+++ lateralus-backend/app/ai/model.py
@@ -3,12 +3,14 @@ from langchain_core.messages import SystemMessage
 from langchain_openai import ChatOpenAI
 from langgraph.graph import MessagesState
 
+from app.ai.tools.schemas import GetWeatherResponse
+
 
 class Model:
     def __init__(self, tools: list):
-        self.model = ChatOpenAI(model='gpt-4o-mini')
+        self.foundation_model = ChatOpenAI(model='gpt-4o-mini')
         self.tools = tools
-        self.bound = self.model.bind_tools(self.tools)
+        self.bound = self.foundation_model.bind_tools(self.tools, response_format=GetWeatherResponse)
 
     def call_model(self, state: MessagesState) -> dict:
         messages = [
